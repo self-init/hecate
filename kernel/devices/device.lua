e3fa8f6b9210f263249d8ee39820544d546b7bc6
self-init <me@williamanderson.dev>
2025-12-25 16:52:25 -0600

    Update device.lua

--- kernel/devices/device.lua
+++ kernel/devices/device.lua
@@ -7,10 +7,46 @@ function Device:new(name, type, bus)
         name = name,
         type = type,
         bus = bus,
+        dev_type = nil,
         driver = nil,
     }
     setmetatable(device, self)
     self.__index = self
 
     return device
+end
+
+function Device:open(inode, mode)
+    if self.driver == nil then
+        error("Error: " .. self.name .. " open: No driver is associated with this device.")
+    end
+    self.driver:open(self, inode, mode)
+end
+
+function Device:read_bytes(inode, bytes)
+    if self.driver == nil then
+        error("Error: " .. self.name .. " read_bytes: No driver is associated with this device.")
+    end
+    self.driver:read_bytes(self, inode, bytes)
+end
+
+function Device:write_bytes(inode, bytes)
+    if self.driver == nil then
+        error("Error: " .. self.name .. " write_bytes: No driver is associated with this device.")
+    end
+    self.driver:write_bytes(self, inode, bytes)
+end
+
+function Device:get_available_bytes(inode, bytes)
+    if self.driver == nil then
+        error("Error: " .. self.name .. " get_available_bytes: No driver is associated with this device.")
+    end
+    self.driver:get_available_bytes(self, inode, bytes)
+end
+
+function Device:close(inode)
+    if self.driver == nil then
+        error("Error: " .. self.name .. " close: No driver is associated with this device.")
+    end
+    self.driver:close(self, inode)
 end
