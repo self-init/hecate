5dfe76bae21fb603644eef650ce9473050de030b
self-init <me@williamanderson.dev>
2025-12-25 17:21:44 -0600

    Update cc_startup.lua

--- kernel/cc_startup.lua
+++ kernel/cc_startup.lua
@@ -1,7 +1,11 @@
 local function main()
     term.clear()
     term.setCursorPos(1,1)
-    Kernel.new(Kernel.ARCH_COMPUTERCRAFT).start()
+    local kernel = Kernel.new(Kernel.ARCH_COMPUTERCRAFT)
+    local ccbus = Bus:new("cc_bus")
+    kernel:register_bus(ccbus)
+    kernel:register_driver(ComputerCraftBus)
+    kernel:start()
 end
 
 main()
