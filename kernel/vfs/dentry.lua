70ae9603b4e7772a0a00dc307e74751b7953ca36
self-init <me@williamanderson.dev>
2025-12-25 19:35:31 -0600

    Rename dentry -> Dentry

--- kernel/vfs/dentry.lua
+++ kernel/vfs/dentry.lua
@@ -1,14 +1,10 @@
 -- A dentry object associates an inode with a path in the virtual file system.
-local Dentry
+local Dentry = {}
 
-local function _dentry_new(ind, parent, name)
+function Dentry.new(ind, parent, name)
     return {
         inode = ind,
         parent = parent,
         name  = name
     }
 end
-
-Dentry = {
-    new = _dentry_new
-}
