9ed9ae1e00f41ca1625016bcb67745199337ae94
self-init <me@williamanderson.dev>
2025-12-25 19:38:23 -0600

    Update vfs.lua
    started work on namei and get_filesystem functions

--- kernel/vfs/vfs.lua
+++ kernel/vfs/vfs.lua
@@ -2,6 +2,7 @@ local Vfs = {}
 
 function Vfs:new()
     local vfs = {
+        mounts = {},
         inode_cache = {},
         dentry_cache = {}
     }
@@ -9,4 +10,15 @@ function Vfs:new()
     self.__index = self
 
     return vfs
+end
+
+-- Gets an Inode from the path and working directory.
+function Vfs:namei(path, working_directory)
+    local working_directory = working_directory or ""
+
+end
+
+-- Get the filesystem/device that a file is from.
+function Vfs:get_filesystem(path, working_directory)
+
 end
