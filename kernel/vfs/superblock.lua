8f4d297c382dce3912dd90726161693e5a45d804
self-init <me@williamanderson.dev>
2025-12-27 00:36:22 -0600

    Update superblock.lua
    Added fs_driver argument.

--- kernel/vfs/superblock.lua
+++ kernel/vfs/superblock.lua
@@ -1,9 +1,10 @@
 local Superblock = {}
 
-function Superblock.create(device, mount_path, root_inode)
+function Superblock.create(device, mount_path, root_inode, fs_driver)
     return {
         mnt = mount_path,
         device = device,
-        root_inode = root_inode
+        root_inode = root_inode,
+        fs_driver = fs_driver,
     }
 end
