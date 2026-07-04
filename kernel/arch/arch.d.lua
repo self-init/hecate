46a3cecbd256a393c1092960b032b858e1fdddaa
self-init <me@williamanderson.dev>
2026-07-04 11:45:58 -0500

    Added documentation for arch.d.lua - closes #15

--- kernel/arch/arch.d.lua
+++ kernel/arch/arch.d.lua
@@ -1,5 +1,11 @@
 ---@meta
 
+---Base class for kernel architectures
 ---@class Arch
----@field init function
----@field step function
+local Arch = {}
+
+---Called on kernel initialization, creates initial devices and filesystems
+function Arch:init() end
+
+---Called on each step of the process scheduler, performs some device upkeep
+function Arch:step() end
