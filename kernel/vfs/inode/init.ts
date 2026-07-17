9f7fae7be7f88900eb24971c43413364036cc03c
self-init <me@williamanderson.dev>
2026-07-17 00:08:28 -0500

    Migrate inode lua -> TS

---
+++ kernel/vfs/inode/init.ts
@@ -0,0 +1,59 @@
+/** @noSelfInFile */
+import { band, bnot, bor, btest } from "../../common/bitwise";
+
+type Inode = {
+	id: integer;
+	links: integer;
+	size: integer;
+	refs: integer;
+	unlinked: boolean;
+	owner: integer;
+	group: integer;
+	mode: integer;
+}
+
+export function create(id: integer, file_type: InodeModeFlags, owner: integer, group: integer): Inode {
+	let default_mode: integer = bor(
+		InodeModeFlags.MASK_READ, InodeModeFlags.MASK_WRITE
+	);
+
+	if (file_type === InodeModeFlags.TYPE_DIR) {
+		default_mode = InodeModeFlags.MASK_PERMS
+	}
+
+	return {
+		id: id,
+		links: 1,
+		size: 0,
+		refs: 0,
+		unlinked: false,
+		owner: owner,
+		group: group,
+		mode: bor(file_type, default_mode)
+	}
+}
+
+export function get_perms(inode: Inode, rew_mask: integer, uid: integer, gids: integer[]): boolean {
+	let perms: integer = inode.mode;
+	if (uid !== inode.owner) {
+		perms = band(perms, bnot(InodeModeFlags.MASK_OWNER))
+	}
+
+	let is_in_group: boolean = false;
+	for (const gid of gids) {
+		if (gid === inode.group) {
+			is_in_group = true;
+			break;
+		}
+	}
+
+	if (!is_in_group) {
+		perms = band(perms, bnot(InodeModeFlags.MASK_GROUP))
+	}
+
+	return btest(perms, rew_mask);
+}
+
+export function get_file_type(inode: Inode, flag: integer) {
+	return btest(inode.mode, flag);
+}
