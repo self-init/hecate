516159d7298e3bbe92c5714c50c33fa7b4cba035
self-init <me@williamanderson.dev>
2026-07-22 23:13:08 -0500

    Migrate KBD drivers lua -> TS

---
+++ kernel/drivers/cc/kbd.ts
@@ -0,0 +1,22 @@
+import { Inode } from "../../vfs/inode/init";
+import { BaseChrDev } from "../common/basechrdev";
+
+export class Kbd extends BaseChrDev {
+	event_queue: string[] = [];
+
+	push_event(event: string) {
+		this.event_queue.push(event);
+	}
+
+	read_file(inode: Inode, offset: integer, length: integer): string {
+		return this.arch.kbd_queue.shift();
+	}
+
+	write_file(inode: Inode, offset: integer, data: unknown): integer {
+		return 0;
+	}
+
+	ioctl(inode: Inode, request: integer, arg: any): unknown {
+		return;
+	}
+}
